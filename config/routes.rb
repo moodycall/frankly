bcb3774e6f42e206d338ab7af8f8c9108e434d07
Morgan Lopes <lopes.morgan@gmail.com>
2014-12-11 18:13:36 -0500

    Add ability to add user info on registration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :counselors
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
   
   root "counselors#index"
 end
