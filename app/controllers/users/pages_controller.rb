d903072810ddc07b36cf70dca42c6b45f5c66efd
Morgan Lopes <lopes.morgan@gmail.com>
2014-12-11 18:36:14 -0500

    Setup user dashboard.

---
+++ app/controllers/users/pages_controller.rb
@@ -0,0 +1,7 @@
+class Users::PagesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def dashboard
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :counselors
 
   devise_for :users, :controllers => {:registrations => "users/registrations"}
+  get 'dashboard' => 'users/pages#dashboard', as: :user_dashboard
   
   root "counselors#index"
 end
