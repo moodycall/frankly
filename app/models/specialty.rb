7649c470e9fb7bcc4669f3154673358ea5a77abe
Morgan J Lopes <morganjlopes@gmail.com>
2015-03-24 11:06:04 -0400

    Make sure only staffed_specialties are visible for search.

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -4,4 +4,10 @@ class Specialty < ActiveRecord::Base
 
 	has_many :specializations
 	has_many :counselors, :through => :specializations
+
+  def self.staffed_specialties
+    self.where(:is_active => true).select do |specialty|
+      specialty.counselors.count > 0
+    end
+  end
 end
