3df0cde3e0c965e78cb818e6dfa0e79bc23e5cff
Morgan J Lopes <morganjlopes@gmail.com>
2015-04-15 09:00:53 -0400

    Change new_user_reg notice for new counselors.

--- app/controllers/counselors_controller.rb
+++ app/controllers/counselors_controller.rb
@@ -206,7 +206,7 @@ class CounselorsController < ApplicationController
     if user_signed_in?
       super
     else
-      redirect_to new_user_registration_path, :notice => 'if you want to add a notice'
+      redirect_to new_user_registration_path, :notice => "Let's get you started. Please enter your name, email, and password."
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
