412b91d451603e31d53caf09fd488a7b16630a1e
vivek <info@moodycall.org>
2016-06-09 18:11:01 +0530

    B055 - Counselor Payouts Issue

--- app/models/concerns/stripe_interactions.rb
+++ app/models/concerns/stripe_interactions.rb
@@ -3,7 +3,7 @@ module StripeInteractions
 
   def charge_customer(user_id, total_in_cents, description)
     user = User.find(user_id)
-    if user.stripe_customer_id != ''
+    if !user.stripe_customer_id.nil?
       # customer = Stripe::Customer.retrieve("#{user.stripe_customer_id}")
 
       begin
