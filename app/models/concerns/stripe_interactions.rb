b4a24842a9cc4b1d6bfca7c31a6b49af3e352ec2
vivek <info@moodycall.org>
2016-06-10 12:18:21 +0530

    B055 - Counselor Payouts Issue

--- app/models/concerns/stripe_interactions.rb
+++ app/models/concerns/stripe_interactions.rb
@@ -3,29 +3,27 @@ module StripeInteractions
 
   def charge_customer(user_id, total_in_cents, description)
     user = User.find(user_id)
-    if !user.stripe_customer_id.nil?
-      customer = Stripe::Customer.retrieve("#{user.stripe_customer_id}")
+    customer = Stripe::Customer.retrieve("#{user.stripe_customer_id}")
 
-      begin
+    begin
 
-        charge = Stripe::Charge.create(
-          :amount => total_in_cents,
-          :currency => "usd",
-          :customer => customer,
-          :description => "#{description}"
-        )
+      charge = Stripe::Charge.create(
+        :amount => total_in_cents,
+        :currency => "usd",
+        :customer => customer,
+        :description => "#{description}"
+      )
 
-        self.stripe_charge_id              = charge.id
-        self.stripe_balance_transaction_id = charge.balance_transaction
-        self.save
+      self.stripe_charge_id              = charge.id
+      self.stripe_balance_transaction_id = charge.balance_transaction
+      self.save
 
-      rescue Stripe::CardError
-        # Send error if no card if on file :: Cannot charge a customer that has no active card
-        unless user.sent_initial_cc_request_dts.present?
-          UserMailer.provide_cc_info(user_id).deliver
-          user.sent_initial_cc_request_dts = Time.now
-          user.save
-        end
+    rescue Stripe::CardError
+      # Send error if no card if on file :: Cannot charge a customer that has no active card
+      unless user.sent_initial_cc_request_dts.present?
+        UserMailer.provide_cc_info(user_id).deliver
+        user.sent_initial_cc_request_dts = Time.now
+        user.save
       end
     end
   end
