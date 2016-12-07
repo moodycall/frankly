33b54823fc079c5671774ebe280168ae0c453dc2
vivek <info@moodycall.org>
2016-12-07 16:42:40 +0530

    make it better

--- app/models/counselor.rb
+++ app/models/counselor.rb
@@ -204,8 +204,8 @@ class Counselor < ActiveRecord::Base
 
 	def is_available_at(time)
 		where       = "active=true and available_datetime = '#{time}'"
-  		available 	= availability_days.select("available_datetime").where("#{where}")
-  		available.present?
+  		available 	= availability_days.select("available_datetime").where("#{where}").present?
+  		available
   		true
 	end
 
